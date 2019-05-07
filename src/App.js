5a53f824f0031b9534ad3af4f43b8680f6135089
Giovanny Rivera <eddyrivera@iMac-de-Eddy-Rivera.local>
2019-05-07 17:27:56 -0300

    day 2 first part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { Router } from "@reach/router";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1>Adopt me!</h1>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 };
 
